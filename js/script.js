13f67631c85bf7b92d86b8dd50029f6510555f21
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-11-07 15:21:08 +0545

    (#4)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-const button = document.getElementById('button');
+const add = document.getElementById('add');
 const taskList = document.getElementById('task-list');
 const input = document.querySelector('.container input')
 
-button.addEventListener('click', () =>{
+function addTask () {
     const taskText = input.value.trim();
     if (taskText === "") {
+        return;
+    }
+
+    if (taskList.children.length >= 3) {
+        const messages = [
+            "Choose only three tasks that matter the most today!",
+            "Make it three so you won't regret doing nothing later!",
+            "Focus on these three—are you ready to complete them?",
+            "Less is more: pick three and give them your best shot!",
+            "Three tasks today = a productive, regret-free day!",
+            "Prioritize your day: finish these three and win!"
+        ];
+
+
+        const randomMessage = messages[Math.floor(Math.random()* messages.length)];
+        alert(randomMessage);
         return;
     }
 
@@ -14,6 +30,13 @@ button.addEventListener('click', () =>{
     taskList.appendChild(li);
 
     input.value = '';
-    
+}
+
+add.addEventListener('click', addTask);
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') addTask();
 });
+
+
 
